c8bb74579227c3d9703a4331b27059005f96c107
Tamas Kocsis <kocsis.tamas88@gmail.com>
2014-12-13 19:29:08 +0100

    trying to debug devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019150355) do
+ActiveRecord::Schema.define(version: 20141020214115) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
